ba2ff8275c5edec7172e164608e38aa15af63475
queue <angeltrisix@gmail.com>
2024-05-14 16:05:43 -0500

    Add early CosJel stuff

---
+++ CosmicSludgeBomb.cs
@@ -0,0 +1,140 @@
+﻿using Terraria;
+using System;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using System.Collections.Generic;
+
+namespace ITDQTest.Projectiles
+{
+    public class CosmicSludgeBomb : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5; // The length of old position to be recorded
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 0; // The recording mode
+            Main.projFrames[Projectile.type] = 7;
+        }
+
+        int defaultWidthHeight = 8;
+        int explosionWidthHeight = 200;
+        public override void SetDefaults()
+        {
+            Projectile.width = defaultWidthHeight; Projectile.height = defaultWidthHeight;
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 300;
+            Projectile.light = 0.5f;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            DrawOffsetX = -16;
+            DrawOriginOffsetY = -16;
+            Projectile.hide = true;
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCsAndTiles.Add(index);
+        }
+
+        bool isStuck = false;
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (Projectile.ai[1] != 0)
+            {
+                return true;
+            }
+            Projectile.velocity = Vector2.Zero;
+            isStuck = true;
+            return false;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Main.instance.LoadProjectile(Projectile.type);
+            Texture2D texture = Mod.Assets.Request<Texture2D>("Projectiles/CosmicSludgeBomb_Effect").Value;
+            Vector2 drawOrigin = new Vector2(texture.Width * 0.5f, Projectile.height * 0.5f);
+            for (int k = 0; k < Projectile.oldPos.Length; k++)
+            {
+                Vector2 drawPos = (Projectile.oldPos[k] - Main.screenPosition) + drawOrigin + new Vector2(0f, Projectile.gfxOffY + DrawOriginOffsetX) + new Vector2(DrawOffsetX, DrawOriginOffsetY) + new Vector2(4, 4);
+                Color color = Projectile.GetAlpha(lightColor) * ((Projectile.oldPos.Length - k) / (float)Projectile.oldPos.Length);
+                Main.EntitySpriteDraw(texture, drawPos, null, color, Projectile.rotation, drawOrigin, Projectile.scale, SpriteEffects.None, 0);
+            }
+
+            return true;
+        }
+        public override void AI()
+        {
+            if (Projectile.timeLeft <= 3)
+            {
+                Projectile.tileCollide = false;
+                Projectile.alpha = 255;
+                Projectile.Resize(explosionWidthHeight, explosionWidthHeight);
+                Projectile.damage = 25;
+                Projectile.knockBack = 10;
+            }
+            if (isStuck == false)
+            {
+                Projectile.velocity.Y = Projectile.velocity.Y + 0.2f;
+            }
+            if (++Projectile.frameCounter >= 3)
+            {
+                Projectile.frameCounter = 0;
+                Projectile.frame = ++Projectile.frame % Main.projFrames[Projectile.type];
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item14, Projectile.position);
+            // Smoke Dust spawn
+            for (int i = 0; i < 50; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default, 2f);
+                dust.velocity *= 1.4f;
+            }
+
+            // Fire Dust spawn
+            for (int i = 0; i < 80; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.ShimmerTorch, 0f, 0f, 100, default, 3f);
+                dust.noGravity = true;
+                dust.velocity *= 5f;
+                dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.ShimmerSpark, 0f, 0f, 100, default, 2f);
+                dust.velocity *= 3f;
+            }
+
+            // Large Smoke Gore spawn
+            for (int g = 0; g < 2; g++)
+            {
+                var goreSpawnPosition = new Vector2(Projectile.position.X + Projectile.width / 2 - 24f, Projectile.position.Y + Projectile.height / 2 - 24f);
+                Gore gore = Gore.NewGoreDirect(Projectile.GetSource_FromThis(), goreSpawnPosition, default, Main.rand.Next(61, 64), 1f);
+                gore.scale = 1.5f;
+                gore.velocity.X += 1.5f;
+                gore.velocity.Y += 1.5f;
+                gore = Gore.NewGoreDirect(Projectile.GetSource_FromThis(), goreSpawnPosition, default, Main.rand.Next(61, 64), 1f);
+                gore.scale = 1.5f;
+                gore.velocity.X -= 1.5f;
+                gore.velocity.Y += 1.5f;
+                gore = Gore.NewGoreDirect(Projectile.GetSource_FromThis(), goreSpawnPosition, default, Main.rand.Next(61, 64), 1f);
+                gore.scale = 1.5f;
+                gore.velocity.X += 1.5f;
+                gore.velocity.Y -= 1.5f;
+                gore = Gore.NewGoreDirect(Projectile.GetSource_FromThis(), goreSpawnPosition, default, Main.rand.Next(61, 64), 1f);
+                gore.scale = 1.5f;
+                gore.velocity.X -= 1.5f;
+                gore.velocity.Y -= 1.5f;
+            }
+            // reset size to normal width and height.
+            Projectile.Resize(defaultWidthHeight, defaultWidthHeight);
+        }
+    }
+}
---
+++ CosmicVoidShard.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using System;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+
+namespace ITDQTest.Projectiles
+{
+    public class CosmicVoidShard : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 14; Projectile.height = 28;
+            Projectile.friendly = false;
+            Projectile.hostile = true;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 400;
+            Projectile.light = 0.5f;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = true;
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.Kill();
+            return false;
+        }
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Dust.NewDust(Projectile.Center, 8, 8, DustID.PortalBolt, Projectile.velocity.X, Projectile.velocity.Y, 0, Color.White, 1);
+            }
+            SoundEngine.PlaySound(SoundID.Item27, Projectile.position);
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+    }
+}
