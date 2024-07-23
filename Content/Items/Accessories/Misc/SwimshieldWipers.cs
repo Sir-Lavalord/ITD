cd3c693f70ed164463890b6f16d0418d368aa593
LolXD8783 <lelomlalol@gmail.com>
2024-07-23 01:43:55 -0600

    Swimshield wipers item

---
+++ Content/Items/Accessories/Misc/SwimshieldWipers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Items.Accessories.Misc
+{
+    public class SwimshieldWipers : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToAccessory(26, 20);
+        }
+        // TODO: should other blindness mods buffs like calamity's be included?
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.buffImmune[BuffID.Darkness] = true;
+            player.buffImmune[BuffID.Blackout] = true;
+        }
+    }
+}
