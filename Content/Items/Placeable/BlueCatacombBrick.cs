fa532cdd293dc6fe6c309f153eed6764c680c51a
Sir Lavalord <146274865+Sir-Lavalord@users.noreply.github.com>
2024-08-06 00:48:21 -0600

    Added Catacomb Brick items.

---
+++ Content/Items/Placeable/BlueCatacombBrick.cs
@@ -0,0 +1,20 @@
+﻿using ITD.Content.Tiles.LayersRework;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Items.Placeable
+{
+    public class BlueCatacombBrick : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<BlueCatacombBrickTile>());
+            Item.width = 12;
+            Item.height = 12;
+        }
+    }
+}
---
+++ Content/Items/Placeable/PinkCatacombBrick.cs
@@ -0,0 +1,20 @@
+﻿using ITD.Content.Tiles.LayersRework;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Items.Placeable
+{
+    public class PinkCatacombBrick : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<PinkCatacombBrickTile>());
+            Item.width = 12;
+            Item.height = 12;
+        }
+    }
+}
