74519d62889db555f4bae388fb6b1e9167d6f1c4
Cowsgomoo48 <prefernottosayno@gmail.com>
2024-08-06 12:02:13 -0500

    Add files via upload
    Added Blueshroom Stem. Current item sprite is horrible, but will probably be updated later.

---
+++ BlueshroomStem.cs
@@ -0,0 +1,22 @@
+﻿using ITD.Content.Tiles.LayersRework;
+using Terraria.ModLoader;
+using Terraria;
+using ITD.Content.Tiles.BlueshroomGroves;
+
+namespace ITD.Content.Items.Placeable
+{
+    public class BlueshroomStem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<BlueshroomStemTile>());
+            Item.width = 12;
+            Item.height = 12;
+        }
+    }
+}
