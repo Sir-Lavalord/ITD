44afca5fc7fda3f51dbadea97e0d3ddf3ac8be2c
Sir Lavalord <146274865+Sir-Lavalord@users.noreply.github.com>
2024-08-06 10:54:38 -0600

    Added Mossy Green Catacomb Bricks.

---
+++ Content/Tiles/Catacombs/MossyGreenCatacombBrickTile.cs
@@ -0,0 +1,24 @@
+﻿using ITD.Content.Dusts;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ITD.Content.Tiles.Catacombs
+{
+    public class MossyGreenCatacombBrickTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            HitSound = SoundID.Item101;
+            DustType = DustID.Shadowflame;
+            AddMapEntry(new Color(69, 120, 80));
+            MinPick = 120;
+        }
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
