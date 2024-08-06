e67e69a96e018459a2459670a48ba6c66c7ab094
Sir Lavalord <146274865+Sir-Lavalord@users.noreply.github.com>
2024-08-06 12:40:40 -0600

    slight edit

---
+++ Content/Walls/Catacombs/PinkCatacombBrickWallTile.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ITD.Content.Walls.Catacombs
+{
+    public class GreenCatacombBrickWall : ModWall
+    {
+        public override void SetStaticDefaults()
+        {
+            AddMapEntry(new Color(17, 31, 42));
+        }
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+    }
+}
