5bdcb7eb334c431134d62961feb7fa6a9365091e
queue <angeltrisix@gmail.com>
2024-08-06 21:21:24 -0500

    Add Catacomb chairs and tables

---
+++ Content/Items/Placeable/CatacombTable.cs
@@ -0,0 +1,33 @@
+﻿using ITD.Content.Tiles.Furniture.Catacombs;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Items.Placeable
+{
+    public class BlueCatacombTable : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<CatacombTableTile>());
+            Item.width = 38;
+            Item.height = 24;
+        }
+    }
+    public class GreenCatacombTable : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<CatacombTableTile>(), 1);
+            Item.width = 38;
+            Item.height = 24;
+        }
+    }
+    public class PinkCatacombTable : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<CatacombTableTile>(), 2);
+            Item.width = 38;
+            Item.height = 24;
+        }
+    }
+}
---
+++ Content/Tiles/Furniture/Catacombs/CatacombTableTile.cs
@@ -0,0 +1,14 @@
+﻿using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace ITD.Content.Tiles.Furniture.Catacombs
+{
+    public class CatacombTableTile : ITDTable
+    {
+        public override void SetStaticTableDefaults()
+        {
+            DustType = DustID.Shadowflame;
+            MapColor = new Color(30, 63, 96);
+        }
+    }
+}
