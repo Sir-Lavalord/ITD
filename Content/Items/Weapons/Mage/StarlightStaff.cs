0583e7cf4081f589b2fd33802aed8ea215aba944
A5torm <nuubassboi@gmail.com>
2024-08-21 00:39:16 +0700

    Starlight Staff, Glowmask

---
+++ Content/Items/Weapons/Mage/StarlightStaff.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using ITD.Content.Projectiles.Friendly.Misc;
+using ITD.Systems;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ITD.Content.Items.Weapons.Mage
+{
+    public class StarlightStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            HeldItemLayer.RegisterData(Item.type, new DrawLayerData()
+            {
+                Texture = ModContent.Request<Texture2D>(Texture + "_Glow"),
+                Color = () => new Color(255, 255, 255, 50) * 0.7f
+            });
+            Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+            Item.staff[Type] = true;
+        }
+        //No Glowmask Yet
+        public override void SetDefaults()
+        {
+            //Change later
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 16;
+            Item.mana = 8;
+            Item.width = 56;
+            Item.height = 56;
+            Item.useTime = 14;
+            Item.useAnimation = 14;
+            Item.useStyle = 5;
+            Item.noMelee = true;
+            Item.knockBack = 1f;
+            Item.value = Item.buyPrice(silver: 10);
+            Item.rare = 3;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.shootSpeed = 8f;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            int proj = Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<StarlightProj>(), damage, knockback, player.whoAmI);
+            return false;
+        }
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+        {
+            Texture2D texture = (Texture2D)Mod.Assets.Request<Texture2D>("Content/Items/Weapons/Mage/StarlightStaff_Glow");
+
+            spriteBatch.Draw(texture, new Vector2(Item.position.X - Main.screenPosition.X + Item.width * 0.5f, Item.position.Y - Main.screenPosition.Y + Item.height - texture.Height * 0.5f),
+                new Rectangle(0, 0, texture.Width, texture.Height), Color.White, rotation, texture.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+        }
+    }
+}
