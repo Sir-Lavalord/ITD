32d328265086f30163b4b03b8d78231588970629
queue <angeltrisix@gmail.com>
2024-08-23 09:51:09 -0500

    Readablity (DustID stuff), Update es-ES

--- Content/Items/Weapons/Mage/ThunderStaff.cs
+++ Content/Items/Weapons/Mage/ThunderStaff.cs
@@ -13,12 +13,6 @@ namespace ITD.Content.Items.Weapons.Mage
     {
         public override void SetDefaults()
         {
-            //Still available for revert
-            /*            Item.DefaultToMagicWeapon(ProjectileID.None, 30, 0f, true);
-                        Item.damage = 300;
-                        Item.staff[Type] = true;
-                        Item.useTurn = true;
-                        Item.shoot = ModContent.ProjectileType<LightningStaffStrike2>();*/
             Item.DamageType = DamageClass.Magic;
             Item.damage = 300;
             Item.mana = 10;
@@ -26,7 +20,7 @@ namespace ITD.Content.Items.Weapons.Mage
             Item.height = 98;
             Item.useTime = 30;
             Item.useAnimation = 30;
-            Item.useStyle = 5;
+            Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 3;
             Item.value = Item.buyPrice(gold: 5);
@@ -41,17 +35,6 @@ namespace ITD.Content.Items.Weapons.Mage
         }
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            /*			Vector2 startPos = new(Main.MouseWorld.X + Main.rand.NextFloat(-256f, 256f), Main.screenPosition.Y);
-                        Vector2 theoreticalEndPos = Helpers.QuickRaycast(new Vector2 (Main.MouseWorld.X, Main.screenPosition.Y), Vector2.UnitY);
-                        Vector2 endPos = Helpers.QuickRaycast(startPos, (theoreticalEndPos - startPos).SafeNormalize(Vector2.Zero), true);
-                        MiscHelpers.CreateLightningEffects(startPos, endPos);
-                        Projectile.NewProjectile(source, endPos, Vector2.Zero, type, damage, knockback, player.whoAmI);
-                        SoundEngine.PlaySound(SoundID.Item89, endPos);
-                        Collision.HitTiles(endPos - new Vector2(32, 0), Vector2.UnitY, 64, 16);
-                        for (int i = 0; i < 5; i++)
-                        {
-                            Gore.NewGore(Item.GetSource_FromThis(), endPos, new Vector2(Main.rand.NextFloat(-2, 2), -1), Main.rand.Next(61, 64));
-                        }*/
             Vector2 vSpawnpos = new Vector2(Main.MouseWorld.X,player.Center.Y - 500);
             Vector2 vMousepos = Main.MouseWorld;
             
@@ -60,15 +43,15 @@ namespace ITD.Content.Items.Weapons.Mage
 
             float ai = Main.rand.Next(200,400);
 
-                int projID = Projectile.NewProjectile(Item.GetSource_FromThis(),vSpawnpos,new Vector2(0,-20),
-                  ModContent.ProjectileType<Projectiles.Friendly.LightningStaffStrike2>(), Item.damage, Item.knockBack, player.whoAmI,
+                Projectile proj = Projectile.NewProjectileDirect(Item.GetSource_FromThis(),vSpawnpos,new Vector2(0,-20),
+                  ModContent.ProjectileType<LightningStaffStrike>(), Item.damage, Item.knockBack, player.whoAmI,
                   rotationVector2.ToRotation(), ai);
-            Main.projectile[projID].tileCollide = false;
-            Main.projectile[projID].scale = 2f;
-            Main.projectile[projID].penetrate = 4;
-            Main.projectile[projID].timeLeft = 190;
-            Main.projectile[projID].CritChance = player.GetWeaponCrit(player.HeldItem);
-            Main.projectile[projID].DamageType = DamageClass.Magic;
+            proj.tileCollide = false;
+            proj.scale = 2f;
+            proj.penetrate = 4;
+            proj.timeLeft = 190;
+            proj.CritChance = player.GetWeaponCrit(player.HeldItem);
+            proj.DamageType = DamageClass.Magic;
                 return false;
         }
     }
