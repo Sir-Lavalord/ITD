8b348042372358fb34432db8a9455c5894523866
duckman723 <107318587+duckman723@users.noreply.github.com>
2024-08-31 14:02:25 +0200

    Added crimson and Corrupt Antidote

---
+++ Content/Items/Accessories/Misc/CorruptAntidote.cs
@@ -0,0 +1,91 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace ITD.Content.Items.Accessories.Misc
+{
+    internal class CorruptAntidote : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+
+            Item.rare = ItemRarityID.Cyan;
+            Item.value = Item.sellPrice(gold: 3);
+
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<CorruptAntidotePlayer>().hasCorruptAntidote = true;
+            player.manaSickReduction *= 0.85f;
+            
+        }
+    }
+
+    internal class CorruptAntidotePlayer : ModPlayer
+    {
+        public bool hasCorruptAntidote;
+        public bool consumedManaPotion;
+        public int buffTimer;
+
+        public override void ResetEffects()
+        {
+            hasCorruptAntidote = false;
+        }
+
+        public override void GetHealMana(Item item, bool quickHeal, ref int healValue)
+        {
+            if (hasCorruptAntidote)
+            {
+                healValue = (int)(healValue * 1.2f);
+            }
+        }
+
+        public override void PreUpdate()
+        {
+            if (consumedManaPotion && hasCorruptAntidote)
+            {
+                buffTimer = 60 * 4;
+                consumedManaPotion = false;
+            }
+            else
+            {
+                consumedManaPotion = false;
+            }
+        }
+
+        public override void UpdateEquips()
+        {
+            if (buffTimer > 0)
+            {
+                Player.GetDamage(DamageClass.Magic) *= 1.25f;
+                Player.manaCost *= 0.85f;
+                buffTimer--;
+            }
+        }
+    }
+
+    internal class CorruptAntidoteManaItem : GlobalItem
+    {
+
+        public override bool ConsumeItem(Item item, Player player)
+        {
+            if (IsManaItem(item))
+            {
+                player.GetModPlayer<CorruptAntidotePlayer>().consumedManaPotion = true;
+            }
+            return true;
+        }
+
+        private bool IsManaItem(Item item)
+        {
+            return item.type == ItemID.LesserManaPotion ||
+                   item.type == ItemID.ManaPotion ||
+                   item.type == ItemID.GreaterManaPotion ||
+                   item.type == ItemID.SuperManaPotion;
+        }
+    }
+}
---
+++ Content/Items/Accessories/Misc/CrimsonAntidote.cs
@@ -0,0 +1,83 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System.Security.Cryptography.Pkcs;
+
+namespace ITD.Content.Items.Accessories.Misc
+{
+    internal class CrimsonAntidote : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+
+            Item.rare = ItemRarityID.Cyan;
+            Item.value = Item.sellPrice(gold: 3);
+
+            Item.accessory = true;
+        }
+
+        public float buffTimeRemaining;
+        public float actualMaxBuffLenght;
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<CrimsonAntidotePlayer>().hasCrimsonAntidote = true;
+            player.potionDelayTime = (int)(player.potionDelayTime * 0.85f);
+            if (player.HasBuff(BuffID.PotionSickness))
+            {
+                buffTimeRemaining = player.potionDelay;
+                if (player.potionDelay > player.potionDelayTime)
+                {
+                    player.ClearBuff(BuffID.PotionSickness);
+                    player.potionDelay = (int)(player.potionDelay * 0.85f);
+                    player.AddBuff(BuffID.PotionSickness, (int)(buffTimeRemaining * 0.85f));
+                }
+                actualMaxBuffLenght = (int)(buffTimeRemaining / 85 * 100);
+                if (player.pStone)
+                {
+                    actualMaxBuffLenght = (int)(buffTimeRemaining / 85 / 75 * 10000);
+                }
+                player.GetModPlayer<CrimsonAntidotePlayer>().resetToTime = (int)actualMaxBuffLenght;
+            }
+        }
+    }
+
+    internal class CrimsonAntidotePlayer : ModPlayer
+    {
+        public bool hasCrimsonAntidote;
+        public int resetToTime;
+
+        public override void ResetEffects()
+        {
+            hasCrimsonAntidote = false;
+        }
+
+        public override void GetHealLife(Item item, bool quickHeal, ref int healValue)
+        {
+            if (hasCrimsonAntidote)
+            {
+                healValue = (int)(healValue * 1.2f);
+            }
+        }
+
+        public override void UpdateLifeRegen()
+        {
+            if (hasCrimsonAntidote && Player.potionDelay > Player.potionDelayTime - 60 * 4)
+            {
+                Player.lifeRegen += 5;
+            }
+        }
+        public override void PreUpdate()
+        {
+            
+            if (resetToTime != 0 && !hasCrimsonAntidote)
+            {
+                Player.ClearBuff(BuffID.PotionSickness);
+                Player.potionDelay = (int)(resetToTime);
+                Player.AddBuff(BuffID.PotionSickness, (int)(resetToTime));
+                resetToTime = 0;
+            }
+        }
+    }
+}
