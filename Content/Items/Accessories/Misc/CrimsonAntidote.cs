b656bea00fbf777e97b943bc023c7a630ee0ec60
duckman723 <107318587+duckman723@users.noreply.github.com>
2024-09-01 13:08:56 +0200

    Crimson Antidote fix
    potion sickness now changes correctly when (un)equiping), made regeneration buff apply to all healing items, moved most code to the modplayer class for other accessories

--- Content/Items/Accessories/Misc/CrimsonAntidote.cs
+++ Content/Items/Accessories/Misc/CrimsonAntidote.cs
@@ -2,6 +2,8 @@
 using Terraria.ModLoader;
 using Terraria.ID;
 using System.Security.Cryptography.Pkcs;
+using System.Net.Http.Headers;
+using System;
 
 namespace ITD.Content.Items.Accessories.Misc
 {
@@ -17,36 +19,18 @@ namespace ITD.Content.Items.Accessories.Misc
 
             Item.accessory = true;
         }
-
-        public float buffTimeRemaining;
-        public float actualMaxBuffLenght;
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetModPlayer<CrimsonAntidotePlayer>().hasCrimsonAntidote = true;
-            player.potionDelayTime = (int)(player.potionDelayTime * 0.85f);
-            if (player.HasBuff(BuffID.PotionSickness))
-            {
-                buffTimeRemaining = player.potionDelay;
-                if (player.potionDelay > player.potionDelayTime)
-                {
-                    player.ClearBuff(BuffID.PotionSickness);
-                    player.potionDelay = (int)(player.potionDelay * 0.85f);
-                    player.AddBuff(BuffID.PotionSickness, (int)(buffTimeRemaining * 0.85f));
-                }
-                actualMaxBuffLenght = (int)(buffTimeRemaining / 85 * 100);
-                if (player.pStone)
-                {
-                    actualMaxBuffLenght = (int)(buffTimeRemaining / 85 / 75 * 10000);
-                }
-                player.GetModPlayer<CrimsonAntidotePlayer>().resetToTime = (int)actualMaxBuffLenght;
-            }
         }
     }
 
     internal class CrimsonAntidotePlayer : ModPlayer
     {
         public bool hasCrimsonAntidote;
-        public int resetToTime;
+        public int RegBoostTimer;
+        public bool shortSickness = false;
+        public float buffTimeRemaining;
 
         public override void ResetEffects()
         {
@@ -63,21 +47,53 @@ namespace ITD.Content.Items.Accessories.Misc
 
         public override void UpdateLifeRegen()
         {
-            if (hasCrimsonAntidote && Player.potionDelay > Player.potionDelayTime - 60 * 4)
+            if (hasCrimsonAntidote && RegBoostTimer > 0)
             {
                 Player.lifeRegen += 5;
+                RegBoostTimer--;
             }
         }
+
         public override void PreUpdate()
         {
-
-            if (resetToTime != 0 && !hasCrimsonAntidote)
+            if (hasCrimsonAntidote)
             {
-                Player.ClearBuff(BuffID.PotionSickness);
-                Player.potionDelay = resetToTime;
-                Player.AddBuff(BuffID.PotionSickness, resetToTime);
-                resetToTime = 0;
+                if (!shortSickness && Player.potionDelay > 0)
+                {
+                    buffTimeRemaining = Player.potionDelay;
+                    Player.potionDelay = (int)(buffTimeRemaining * 0.85f);
+                    Player.ClearBuff(BuffID.PotionSickness);
+                    Player.AddBuff(BuffID.PotionSickness, Player.potionDelay);
+                    shortSickness = true;
+                }
+             }
+            else
+            {
+                if (shortSickness && Player.potionDelay > 0)
+                {
+                    buffTimeRemaining = Player.potionDelay;
+                    Player.potionDelay = (int)(buffTimeRemaining / 85 * 100);
+                    Player.ClearBuff(BuffID.PotionSickness);
+                    Player.AddBuff(BuffID.PotionSickness, Player.potionDelay);
+                    shortSickness = false;
+                }
             }
+            if (!Player.HasBuff(BuffID.PotionSickness))
+            {
+                shortSickness = false;
+            }
+        }
+    }
+
+    internal class CrimsonAntidoteHealItem : GlobalItem
+    {
+        public override bool? UseItem(Item item, Player player)
+        {
+            if (item.healLife > 0)
+            {
+                player.GetModPlayer<CrimsonAntidotePlayer>().RegBoostTimer = 60 * 4;
+            }
+            return true;
         }
     }
 }
