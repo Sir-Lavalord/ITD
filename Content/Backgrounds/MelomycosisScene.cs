5bf63193a7f4d0a2a6073856384a38759f4e6d3a
queue <angeltrisix@gmail.com>
2024-10-19 15:03:20 -0500

    melomycosis shader

---
+++ Content/Backgrounds/MelomycosisScene.cs
@@ -0,0 +1,20 @@
+﻿using ITD.Content.Buffs.Debuffs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Backgrounds
+{
+    public class MelomycosisScene : ModSceneEffect
+    {
+        public override bool IsSceneEffectActive(Player player) => player.HasBuff<MelomycosisBuff>();
+        public override void SpecialVisuals(Player player, bool isActive)
+        {
+            player.ManageSpecialBiomeVisuals("BlackMold", isActive);
+        }
+    }
+}
