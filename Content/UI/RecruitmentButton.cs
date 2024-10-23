34ed84b23004b5345f4bc53d50a7a4c712f3b8b3
queue <angeltrisix@gmail.com>
2024-10-23 17:42:09 -0500

    handle all edge cases when drawing recruitment button

--- Content/UI/RecruitmentButton.cs
+++ Content/UI/RecruitmentButton.cs
@@ -13,6 +13,9 @@ using ITD.Utilities;
 using ReLogic.Graphics;
 using Terraria.GameContent;
 using ITD.Systems.Recruitment;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using Terraria.UI.Chat;
 
 namespace ITD.Content.UI
 {
@@ -23,13 +26,14 @@ namespace ITD.Content.UI
         private static bool RecruitmentButtonVisible()
         {
             bool talking = Main.LocalPlayer.talkNPC > -1;
+            bool inSpecialNPCTalkMenu = Main.InGuideCraftMenu || Main.InReforgeMenu;
             Mod dialogueTweakMod = ITD.Instance.dialogueTweak;
             if (dialogueTweakMod is null)
-                return talking;
+                return talking && !inSpecialNPCTalkMenu;
             dialogueTweakMod.TryFind("Configuration", out ModConfig config);
             FieldInfo field = config.GetType().GetField("VanillaUI");
             bool isDialogueTweakVanillaUI = (bool)field.GetValue(config);
-            return talking && isDialogueTweakVanillaUI;
+            return talking && !inSpecialNPCTalkMenu && isDialogueTweakVanillaUI;
         }
         public override int InsertionIndex(List<GameInterfaceLayer> layers)
         {
@@ -43,12 +47,18 @@ namespace ITD.Content.UI
         public override void Update(GameTime gameTime)
         {
             DynamicSpriteFont font = FontAssets.MouseText.Value;
-            Vector2 size = font.MeasureString(Main.npcChatText);
-            float wrapWidth = 460f;
-            int numberOfLines = (int)(size.X / wrapWidth);
-            float yOffset = numberOfLines * 30f; // hacky way to do this
-            //Main.NewText(yOffset.ToString() + " " + size.X.ToString());
-            recruitmentButton.UpdateProperties(32f, Main.screenWidth / 2 + 210, 150 + yOffset);
+
+            float xOffset = 0f;
+
+            if (Main.npcChatCornerItem > 0 || ITD.Instance.dialogueTweak != null)
+                xOffset -= 32f;
+
+            // prev implementation was horrid. this replicates what vanilla does to draw Main.npcChatCornerItem. this is more solid
+            List<List<TextSnippet>> lines = Utils.WordwrapStringSmart(Main.npcChatText, Color.White, font, 460, 10);
+            int numberOfLines = lines.Count;
+
+            float yOffset = (numberOfLines + 1) * 30 + 30;
+            recruitmentButton.UpdateProperties(32f, Main.screenWidth / 2 + 210 + xOffset, 60 + yOffset);
             Recalculate();
             base.Update(gameTime);
         }
