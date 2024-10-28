e9a27e8638c2a7588cb4aa648f6d49cf5e411902
queue <angeltrisix@gmail.com>
2024-10-28 17:34:55 -0500

    Update EntityAnim.cs

--- Utilities/EntityAnim/EntityAnim.cs
+++ Utilities/EntityAnim/EntityAnim.cs
@@ -22,8 +22,15 @@ namespace ITD.Utilities.EntityAnim
         /// Keyframes in the animation. These will play in the order they're appended in
         /// </summary>
         public Keyframe<T>[] Keyframes { get; set; } = keyframes;
+        /// <summary>
+        /// The index of the current playing Keyframe in the animation.
+        /// </summary>
         public int FrameIndex { get; set; } = 0;
-        public T PreviousValue { get; set; } = default;
+        private T PreviousValue { get; set; } = default;
+        /// <summary>
+        /// Only use this overload if you need to add Keyframes with different Entity targets into a single animation for whatever reason. Otherwise use the other two overloads.
+        /// If using this overload, create new Keyframes using <see cref="AnimHelpers.CreateFor{T}(Entity, Expression{Func{T}}, Func{T}, int, EasingFunctions.EasingFunc)"/>
+        /// </summary>
         public EntityAnim<T> Append(params Keyframe<T>[] addKeyframes)
         {
             return new EntityAnim<T>([.. Keyframes, .. addKeyframes])
@@ -31,9 +38,21 @@ namespace ITD.Utilities.EntityAnim
                 Entity = Entity,
             };
         }
+        /// <summary>
+        /// Returns a new <see cref="EntityAnim{T}"/> which adds a <see cref="Keyframe{T}"/> with the given parameters to the end of the Keyframes array.
+        /// </summary>
         public EntityAnim<T> Append(Expression<Func<T>> propertyExpr, Func<T> endValue, int frames, EasingFunctions.EasingFunc easingFunc)
         {
             Keyframe<T> newKeyframe = AnimHelpers.CreateFor(Entity, propertyExpr, endValue, frames, easingFunc);
+            return new EntityAnim<T>([.. Keyframes, newKeyframe])
+            {
+                Entity = Entity,
+            };
+        }
+        /// <inheritdoc cref="Append(Expression{Func{T}}, Func{T}, int, EasingFunctions.EasingFunc)"/>>
+        public EntityAnim<T> Append(Expression<Func<T>> propertyExpr, T endValue, int frames, EasingFunctions.EasingFunc easingFunc)
+        {
+            Keyframe<T> newKeyframe = AnimHelpers.CreateFor(Entity, propertyExpr, () => endValue, frames, easingFunc);
             return new EntityAnim<T>([.. Keyframes, newKeyframe])
             {
                 Entity = Entity,
@@ -86,7 +105,7 @@ namespace ITD.Utilities.EntityAnim
 
             for (int i = keyframe; i < Keyframes.Length; i++)
             {
-                ResetKeyframe(keyframes[i]);
+                ResetKeyframe(Keyframes[i]);
             }
 
             if (Keyframes.Length > 0)
