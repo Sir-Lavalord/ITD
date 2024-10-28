a5eb0e27d1264772d399f2d370a6ad62eedafb1d
Cowsgomoo48 <prefernottosayno@gmail.com>
2024-10-28 18:46:32 -0500

    Added Frostgrip

---
+++ Content/Projectiles/Friendly/Melee/Snaptraps/Extra/FrostgripIcicle.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ITD.Utilities;
+
+namespace ITD.Content.Projectiles.Friendly.Melee.Snaptraps.Extra
+{
+    public class FrostgripIcicle : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.width = 12; Projectile.height = 12;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = 3;
+            Projectile.timeLeft = 127;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = true;
+            Projectile.scale = 0.65f;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            Projectile.velocity.Y = Projectile.velocity.Y + 0.25f;
+            if (Projectile.velocity.Y > 16f)
+            {
+                Projectile.velocity.Y = 16f;
+            }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            for (int d = 0; d < 10; d++)
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.SnowflakeIce, 0f, 0f, 150, default(Color), 0.75f);
+            }
+        }
+    }
+}
