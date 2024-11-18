50ef9b97e9420e94e5dd960ce97c5719d8a59c95
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-11-17 18:06:21 -0700

    Add Topaz Necklace outline

---
+++ Content/Items/Accessories/AllAround/TopazNecklace.cs
@@ -0,0 +1,28 @@
+﻿using ITD.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Items.Accessories.AllAround
+{
+    public class TopazNecklace : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 34;
+            Item.height = 34;
+            Item.value = Item.sellPrice(50000);
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            ITDPlayer modPlayer = player.GetModPlayer<ITDPlayer>();
+            // Add stuff for lightning here
+        }
+    }
+}
