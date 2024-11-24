4568cc1796387d08c9c2c6cba9811d8e4182ed21
Cowsgomoo48 <prefernottosayno@gmail.com>
2024-11-23 21:50:16 -0600

    Black Cacophany

---
+++ Content/Items/Weapons/Ranger/BlackCacophany.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using ITD.Systems;
+using ITD.Players;
+using ITD.Utilities;
+using ITD.Content.Projectiles.Friendly.Ranger;
+using ITD.Content.Projectiles.Friendly.Melee;
+using ITD.Content.Items.Favors;
+using Terraria.GameContent.Creative;
+using System;
+using Mono.Cecil;
+using rail;
+using System.Collections.Generic;
+using ITD.Content.Items.Other;
+using Terraria.Localization;
+
+namespace ITD.Content.Items.Weapons.Ranger
+{
+    public class BlackCacophany : ModItem
+    {
+        public int windup = 1;
+        public int mode = 1;
+        public float colorProgress = .02f;
+        public bool isRightClickHeld = false;
+
+        public override void SetStaticDefaults()
+        {
+            FrontGunLayer.RegisterData(Item.type);
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 100;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 20;
+            Item.height = 20;
+            Item.useTime = 60;
+            Item.useAnimation = 60;
+            Item.useStyle = -1;
+            Item.knockBack = 5;
+            Item.value = 500000;
+            Item.rare = ItemRarityID.Cyan;
+            Item.UseSound = SoundID.Item11;
+
+            Item.width = 116;
+            Item.height = 36;
+
+            Item.shoot = ProjectileID.Bullet;
+            Item.useAmmo = AmmoID.Bullet;
+            Item.shootSpeed = 6f;
+            Item.noMelee = true;
+            Item.autoReuse = true;
+            Item.noUseGraphic = true;
+        }
+
+        public void Hold(Player player)
+        {
+            ITDPlayer modPlayer = player.GetITDPlayer();
+            Vector2 mouse = modPlayer.MousePosition;
+
+            if (mouse.X < player.Center.X)
+                player.direction = -1;
+            else
+                player.direction = 1;
+
+            float rotation = (Vector2.Normalize(mouse - player.MountedCenter)*player.direction).ToRotation();
+            player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, rotation * player.gravDir - modPlayer.recoilFront * player.direction - MathHelper.PiOver2 * player.direction);
+            player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, rotation * player.gravDir - modPlayer.recoilBack * player.direction - MathHelper.PiOver2 * player.direction);
+        }
+
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            Hold(player);
+        }
+        public override void HoldStyle(Player player, Rectangle heldItemFrame)
+        {
+            Hold(player);
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-4f, -20f);
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+        int privateMode = 0;
+        public override bool CanUseItem(Player player)
+        {
+            Item.shoot = ProjectileID.None;
+            Item.UseSound = SoundID.Item11;
+
+            if (player.altFunctionUse == 2)
+            {
+                if (mode == 1)
+                {
+                    mode = 2; //Fire state
+                    CombatText.NewText(player.getRect(), new Color(184, 115, 51), "Firing Mode", false, false);
+                }
+                else
+                {
+                    mode = 1; //Windup state
+                    CombatText.NewText(player.getRect(), new Color(145, 145, 145), "Windup Mode", false, false);
+                }
+            }
+            else
+            {
+                if (mode == 1)
+                {
+                    Item.shoot = ProjectileID.None;
+                    Item.useAmmo = AmmoID.None;
+                    Item.useTime = 6;
+                    Item.useAnimation = 6;
+                    SoundStyle blackcacophany = new SoundStyle("ITD/Content/Sounds/BarrelSpinShort") with
+                    {
+                        Volume = 0.75f,
+                        Pitch = 1f * windup / 10,
+                        PitchVariance = 0.1f,
+                        MaxInstances = 3,
+                        SoundLimitBehavior = SoundLimitBehavior.IgnoreNew
+                    };
+                    SoundEngine.PlaySound(blackcacophany, player.Center);
+
+                    windup += 2;
+                    if (windup >= 60)
+                    {
+                        windup = 60;
+                        CombatText.NewText(player.getRect(), new Color(colorProgress, colorProgress, colorProgress), "MAX!", true, false);
+                    }
+                }
+                else
+                {
+                    Item.shoot = ProjectileID.Bullet;
+                    Item.useAmmo = AmmoID.Bullet;
+                    Item.useTime = 8 - windup / 10;
+                    Item.useAnimation = 8 - windup / 10;
+                    if (windup > 2)
+                    {
+                        SoundStyle cacophanyfire = new SoundStyle("ITD/Content/Sounds/BlackCacophanyFire") with
+                        {
+                            Volume = 0.5f,
+                            Pitch = 0.5f,
+                            PitchVariance = 0.1f,
+                            MaxInstances = 3,
+                            SoundLimitBehavior = SoundLimitBehavior.IgnoreNew
+                        };
+                        Item.UseSound = cacophanyfire;
+                        return true;
+                    }
+                    else
+                    {
+                        Item.UseSound = SoundID.Item11;
+                        return false;
+                    }
+                }
+            }
+            return base.CanUseItem(player);
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (mode == 1)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Vector2 trueSpeed = velocity.RotatedByRandom(MathHelper.ToRadians(15));
+                    float scale = Main.rand.NextFloat(.8f, 1.6f);
+                    trueSpeed *= scale;
+
+                    float shellShift = MathHelper.ToRadians(-50);
+                    float SVar = shellShift + MathHelper.ToRadians(Main.rand.Next(-100, 301) / 10);
+                    float Sspeed = .05f * Main.rand.Next(15, 41);
+                    Projectile.NewProjectile(source, position, trueSpeed, type, damage, knockback, player.whoAmI);
+                }
+                colorProgress = .02f;
+                windup -= 1;
+                if (windup < 0)
+                {
+                    windup = 0;
+                }
+                return false;
+            }
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 116f;
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            privateMode = mode;
+            string textColor;
+            if (privateMode == 1)
+            {
+                textColor = "[c/919191:";
+            } 
+            else
+            {
+                textColor = "[c/B87333:";
+            }
+            foreach (TooltipLine line in tooltips)
+            {
+                if (line.Mod == "Terraria" && line.Name == "Tooltip0")
+                {
+                    string Text = Language.GetOrRegister(Mod.GetLocalizationKey($"Items.{nameof(BlackCacophany)}.CacophanyType.{privateMode}")).Value;
+                    line.Text = textColor + line.Text + "]";
+                    line.Text += textColor + " " + Text + "]";
+                }
+                if (line.Mod == "Terraria" && line.Name == "Tooltip4")
+                {
+                    string Text = Language.GetOrRegister(Mod.GetLocalizationKey($"Items.{nameof(BlackCacophany)}.Mode.{privateMode}")).Value;
+                    line.Text = Text + " " + line.Text;//Fine for now
+                }
+            }
+        }
+    }
+}
