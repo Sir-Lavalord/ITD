34091bbfd3971a64b5ff496f3b461508d495a06e
A5torm <nuubassboi@gmail.com>
2024-12-08 23:42:34 +0700

    royal jelly scepter

---
+++ Content/Buffs/Debuffs/RoyalJellyDebuff.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using ITD.Content.Buffs.Debuffs;
+
+namespace ITD.Content.Buffs.Debuffs
+{
+    public class RoyalJellyDebuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            BuffID.Sets.IsATagBuff[Type] = true;
+        }
+        public static int TagDamage = 6;//Raw tag damage, might be op
+    }
+}
+
+public class RoyalJellyDebuffTag : GlobalNPC
+{
+    public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref NPC.HitModifiers modifiers)
+    {
+        if (projectile.npcProj || projectile.trap)
+            return;
+
+
+        var projTagMultiplier = ProjectileID.Sets.SummonTagDamageMultiplier[projectile.type];
+        if (npc.HasBuff<RoyalJellyDebuff>())
+        {
+            modifiers.FlatBonusDamage += RoyalJellyDebuff.TagDamage * projTagMultiplier;
+        }
+    }
+    public override void PostAI(NPC npc)
+    {
+        if (npc.HasBuff<RoyalJellyDebuff>())
+        {
+            npc.color = Color.Orange;
+        }
+        else npc.color = default;
+    }
+}
+
---
+++ Content/Buffs/GeneralBuffs/RoyalJellyBuff.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace ITD.Content.Buffs.GeneralBuffs
+{
+    public class RoyalJellyBuff : ModBuff
+    {
+        public static float DefReduce = 6;
+
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense += 6;
+            player.GetAttackSpeed<MeleeDamageClass>() += 0.05f;
+            player.GetCritChance<RangedDamageClass>() += 0.05f;
+            player.GetDamage<MagicDamageClass>() += 0.05f;
+            player.honey = true;
+            player.honeyWet = true;
+        }
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+        }
+    }
+}
