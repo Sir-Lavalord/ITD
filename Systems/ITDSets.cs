2e1741f445451d0c7f835188700be3aa78a5f934
queue <angeltrisix@gmail.com>
2024-12-29 13:30:03 -0500

    fix

--- Systems/ITDSets.cs
+++ Systems/ITDSets.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using Terraria.ID;
+﻿using Terraria.ID;
 
 namespace ITD
 {
     public class ITDSets
     {
         public static readonly int[] ToScrapeableMoss = TileID.Sets.Factory.CreateIntSet(-1);
-        public static readonly int[] LeafGrowFX = GoreID.Sets.Factory.CreateIntSet(GoreID.TreeLeaf_Normal);
+        public static readonly int[] LeafGrowFX = TileID.Sets.Factory.CreateIntSet(GoreID.TreeLeaf_Normal);
         public static readonly bool[] SnowpoffDiggable = TileID.Sets.Factory.CreateBoolSet(TileID.SnowBlock);
     }
 }
