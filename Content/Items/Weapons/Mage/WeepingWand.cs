b37d96a331e5bfd879ba5b2538acd35acce8f250
Cowsgomoo48 <prefernottosayno@gmail.com>
2025-01-13 17:23:19 -0600

    WeepingWand progress

---
+++ Content/Items/Weapons/Mage/WeepingWand.cs
@@ -0,0 +1,67 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Linq;
+
+using ITD.Content.Items.Placeable;
+using ITD.Utilities;
+using ITD.Content.Projectiles.Friendly.Mage;
+using ITD.Content.Dusts;
+using ITD.Content.Projectiles.Friendly.Melee.Snaptraps.Extra;
+using ITD.Content.Projectiles.Friendly;
+
+
+namespace ITD.Content.Items.Weapons.Mage
+{
+    public class WeepingWand : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 40;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 60;
+            Item.useAnimation = 20;
+            Item.autoReuse = true;
+
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 8;
+            Item.knockBack = 1f;
+            Item.crit = 4;
+            Item.noMelee = true;
+            Item.mana = 6;
+
+            Item.value = Item.buyPrice(silver: 25);
+            Item.rare = 2;
+            Item.UseSound = SoundID.Item71;
+            Item.shoot = ModContent.ProjectileType<WeepWandWisp>();
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.NextBool(3))
+            {
+                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, ModContent.DustType<Dusts.WispDust>());
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 mousePosition = Main.MouseWorld;
+            Vector2 direction = mousePosition - player.position;
+            direction.Normalize();
+            float projectileSpeed = 4f;
+
+            Projectile.NewProjectile(player.GetSource_FromThis(), player.position.X, player.position.Y - 20f, direction.X * projectileSpeed, direction.Y * projectileSpeed, ModContent.ProjectileType<WeepWandWisp>(), 7, 0f, player.whoAmI);
+            Projectile.NewProjectile(player.GetSource_FromThis(), player.position.X, player.position.Y, direction.X * (projectileSpeed * 2f), direction.Y * projectileSpeed, ModContent.ProjectileType<WeepWandWisp>(), 7, 0f, player.whoAmI);
+            Projectile.NewProjectile(player.GetSource_FromThis(), player.position.X, player.position.Y + 20f, direction.X * projectileSpeed, direction.Y * projectileSpeed, ModContent.ProjectileType<WeepWandWisp>(), 7, 0f, player.whoAmI);
+
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/Friendly/Mage/WeepWandWisp.cs
@@ -0,0 +1,113 @@
+﻿using ITD.Content.Dusts;
+using ITD.Utilities;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Projectiles.Friendly.Mage
+{
+    public class WeepWandWisp : ModProjectile
+    {
+        private int DelayTimer = 0;
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 4;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.width = 20;
+            Projectile.height = 72;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 600;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = false;
+
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = (15);
+
+            Projectile.frame = 1;
+        }
+        private NPC HomingTarget
+        {
+            get => Projectile.ai[0] == 0 ? null : Main.npc[(int)Projectile.ai[0] - 1];
+            set
+            {
+                Projectile.ai[0] = value == null ? 0 : value.whoAmI + 1;
+            }
+        }
+
+        public override void AI()
+        {
+            Projectile.damage = 0;
+            float maxDetectRadius = 400f;
+
+            if (DelayTimer < 16)
+            {
+                Projectile.velocity *= 0.85f;
+                DelayTimer += 1;
+                return;
+            } 
+            else
+            {
+                Projectile.velocity *= 1.05f;
+            }
+
+            if (Projectile.frame < 4)
+            {
+                ++Projectile.frame;
+            }
+            else
+            {
+                return;
+            }
+
+            if (Main.rand.NextBool(3))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<WispDust>(), 0f, 0f, 150, default(Color), 0.5f);
+                }
+            }
+
+            Projectile.damage = 7;
+            Projectile.frame = 4;
+
+            if (Projectile.penetrate > 0)
+            {
+                HomingTarget ??= Projectile.FindClosestNPC(maxDetectRadius);
+
+                if (HomingTarget == null)
+                {
+                    return;
+                }
+                else
+                {
+                    //Projectile.alpha = 0;
+                }
+
+                Vector2 directionToTarget = HomingTarget.Center - Projectile.Center;
+                directionToTarget.Normalize();
+
+                float length = Projectile.velocity.Length();
+                float targetAngle = Projectile.AngleTo(HomingTarget.Center);
+                Projectile.velocity = Projectile.velocity.ToRotation().AngleTowards(targetAngle, MathHelper.ToRadians(6)).ToRotationVector2() * length;
+                Projectile.Center += Main.rand.NextVector2Circular(1, 1);
+
+                Projectile.rotation = directionToTarget.ToRotation();
+            }
+            else
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<WispDust>(), 0f, 0f, 150, default(Color), 0.5f);
+                }
+                Projectile.Kill();
+            }
+        }
+    }
+}
