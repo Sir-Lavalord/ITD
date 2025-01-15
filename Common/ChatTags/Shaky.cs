9f26ba63c05838320fd6026da3dac3749085ec4a
queue <angeltrisix@gmail.com>
2025-01-15 18:34:09 -0500

    chat tags completed

--- Common/ChatTags/Shaky.cs
+++ Common/ChatTags/Shaky.cs
@@ -20,11 +20,15 @@ namespace ITD.Common.ChatTags
     public class ShakySnippet : TextSnippet
     {
         public float Strength;
+        private Vector2[] shakes;
+        private uint lastShakeGUC;
         public ShakySnippet(string text, Color baseColor, float strength)
         {
             Text = text;
             Color = baseColor;
             Strength = strength;
+            shakes = new Vector2[Text.Length];
+            lastShakeGUC = 0;
         }
         public override bool UniqueDraw(bool justCheckingString, out Vector2 size, SpriteBatch spriteBatch, Vector2 position = default, Color color = default, float scale = 1)
         {
@@ -34,6 +38,16 @@ namespace ITD.Common.ChatTags
                 return false;
             }
 
+            // to have consistent shakes for every character, even when drawn with shadows, we can pre-compute new shakes for each character every frame.
+            if (lastShakeGUC != Main.GameUpdateCount)
+            {
+                for (int i = 0; i < Text.Length; i++)
+                {
+                    shakes[i] = Main.rand.NextVector2Circular(Strength, Strength);
+                }
+                lastShakeGUC = Main.GameUpdateCount;
+            }
+
             Vector2 outSize = Vector2.Zero;
             DynamicSpriteFont font = FontAssets.MouseText.Value;
             Vector2 currentPosition = position;
@@ -41,7 +55,7 @@ namespace ITD.Common.ChatTags
             for (int i = 0; i < Text.Length; i++)
             {
                 char c = Text[i];
-                Vector2 shake = Main.rand.NextVector2Circular(Strength, Strength);
+                Vector2 shake = shakes[i];
                 string str = c.ToString();
                 Vector2 characterSize = font.MeasureString(str);
                 Vector2 characterPosition = currentPosition + shake;
