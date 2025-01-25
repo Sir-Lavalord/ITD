cf3f4d24eb5a27acee03c796dd425d0c17ee505c
queue <angeltrisix@gmail.com>
2025-01-25 11:09:41 -0500

    networking stuff

--- Content/Events/LavaRainEvent.cs
+++ Content/Events/LavaRainEvent.cs
@@ -1,4 +1,6 @@
 ﻿using ITD.Content.NPCs.Events.LavaRain;
+using ITD.Networking;
+using ITD.Networking.Packets;
 using ITD.Particles;
 using ITD.Particles.Events;
 using System.Collections.Generic;
@@ -10,11 +12,18 @@ namespace ITD.Content.Events
 {
     public class LavaRainEvent : ITDEvent
     {
-        public float Progress = 0f;
+        public ushort maxProgress;
+        public ushort currentProgress;
         public ParticleEmitter rainParticleEmitter;
         public override void OnActivate()
         {
-            if (Main.netMode == NetmodeID.Server)
+            int count = 0;
+            foreach (var plr in Main.ActivePlayers)
+                count++;
+            maxProgress = (ushort)(40 + (20 * count));
+            currentProgress = 0;
+
+            if (Main.dedServ)
                 return;
             rainParticleEmitter = ParticleSystem.NewEmitter<LavaRainParticle>();
             // if this is not set the emitter automatically dies which we don't want
@@ -29,13 +38,23 @@ namespace ITD.Content.Events
             if (Main.rand.NextBool(2))
             {
                 Vector2 emitPosition = (Main.screenPosition - Vector2.UnitX * rangePadding) + new Vector2(Main.rand.NextFloat(Main.screenWidth + rangePadding + float.Epsilon), 0f);
-                Vector2 emitVelocity = Vector2.UnitY * 15f;
+                Vector2 emitVelocity = Vector2.UnitY * 13.5f;
                 rainParticleEmitter?.Emit(emitPosition, emitVelocity, emitVelocity.ToRotation() - MathHelper.PiOver2, 160);
             }
+        }
+        public override float GetVisualProgress()
+        {
+            return currentProgress / (float)maxProgress;
         }
         public override IEnumerable<(int, float)> GetPool(NPCSpawnInfo spawnInfo)
         {
             yield return (ModContent.NPCType<PyroclasticSlime>(), 1f);
+        }
+        public override void OnKill(NPC npc)
+        {
+            if (npc.type == ModContent.NPCType<PyroclasticSlime>())
+                currentProgress++;
+            NetSystem.SendPacket<SyncEventDataPacket>(new(Type));
         }
         public override void OnDeactivate()
         {
