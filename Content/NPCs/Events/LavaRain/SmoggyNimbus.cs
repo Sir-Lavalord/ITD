d69b8fba9ac29f20ea67a8e20961c2d2f163e93f
queue <angeltrisix@gmail.com>
2025-01-28 12:48:37 -0500

    moggling bingus

--- Content/NPCs/Events/LavaRain/SmoggyNimbus.cs
+++ Content/NPCs/Events/LavaRain/SmoggyNimbus.cs
@@ -1,14 +1,115 @@
-﻿using ITD.Utilities.Placeholders;
+﻿using ITD.Content.Projectiles.Hostile;
+using ITD.Utilities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace ITD.Content.NPCs.Events.LavaRain
 {
     public class SmoggyNimbus : ITDNPC
     {
-        public override string Texture => Placeholder.PHBuff;
+        public enum _AIState
+        {
+            Following,
+            Spinning
+        }
+        public ref float AITimer => ref NPC.ai[0];
+        public ref float AILockOnPeriod => ref NPC.ai[1];
+        public ref float AIRand => ref NPC.ai[2];
+        public _AIState AIState { get { return (_AIState)NPC.ai[3]; } set { NPC.ai[3] = (float)value; } }
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 1;
+            ITDSets.LavaRainEnemy[Type] = true;
+        }
+        public override void SetDefaults()
+        {
+            NPC.damage = 30;
+            NPC.width = 50;
+            NPC.height = 40;
+            NPC.defense = 20;
+            NPC.lifeMax = 100;
+            NPC.value = Item.buyPrice(silver: 4);
+            NPC.HitSound = SoundID.NPCHit30;
+            NPC.DeathSound = SoundID.NPCDeath33;
+            NPC.noGravity = true;
+            NPC.lavaImmune = true;
+        }
+        public override void AI()
+        {
+            Dust d = Dust.NewDustDirect(NPC.BottomLeft, NPC.width, 1, DustID.Torch, SpeedY: 5f);
+            d.noGravity = true;
+            if (InvalidTarget)
+                NPC.TargetClosest(false);
+            NPC.spriteDirection = NPC.direction = NPC.velocity.X > 0 ? -1 : 1;
+            // angry nimbus AI seems to be a simple velocity change so it arrives right over the player. if overshot, it recalculates the speed again.
+            // if it's x is within range of the player, it starts shooting the rain projectiles.
+            // we can do something similar except add a little spice to it, maybe add some extra attacks
+            Player target = Main.player[NPC.target];
+            Vector2 targetPosition = target.Center - Vector2.UnitY * 216f;
+            Vector2 toTargetPosition = targetPosition - NPC.Center;
+            Vector2 toTargetPositionNormalized = toTargetPosition.SafeNormalize(Vector2.Zero);
+            if (AIState == _AIState.Following)
+            {
+                // changing the x velo will be a simple smoothstep. for changing y, let's make it a fixed speed so melee users can actually do stuff against the enemy.
+                NPC.velocity.X = MathHelper.Clamp(MathHelper.SmoothStep(NPC.velocity.X, toTargetPosition.X, 0.04f), -6f, 6f);
+                NPC.velocity.Y = toTargetPositionNormalized.Y * 4f;
+                // attacks
+                float range = 50f;
+                float xRemapped = NPC.Center.X - target.Center.X;
+                if (Math.Abs(xRemapped) < range && NPC.position.Y < target.position.Y)
+                {
+                    AILockOnPeriod = 60;
+                }
+            }
+            else
+            {
+                int attackLength = 60;
+                int attackFreq = 10;
+                AITimer++;
+                NPC.rotation = Helpers.Remap(AITimer, 0, attackLength, 0, MathHelper.TwoPi);
+                if (Main.netMode != NetmodeID.MultiplayerClient)
+                {
+                    if (AITimer % attackFreq == 0)
+                    {
+                        Vector2 velo = (NPC.rotation + MathHelper.PiOver2).ToRotationVector2();
+                        Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, velo * 8.5f, ModContent.ProjectileType<SmoggyNimbusRaindrop>(), NPC.damage, 0f, ai0: Main.rand.Next(3));
+                    }
+                }
+                if (AITimer >= attackLength)
+                {
+                    AIState = _AIState.Following;
+                    AIRand = Main.rand.Next(60, 80);
+                    NPC.netUpdate = true;
+                }
+            }
+            if (AILockOnPeriod > 0)
+            {
+                AILockOnPeriod--;
+                if (++AITimer > AIRand)
+                {
+                    AITimer = 0;
+                    AIRand = Main.rand.Next(16, 28);
+                    NPC.netUpdate = true;
+                    if (Main.netMode != NetmodeID.MultiplayerClient)
+                    {
+                        Vector2 projPos = Vector2.Lerp(NPC.BottomLeft, NPC.BottomRight, Main.rand.NextFloat());
+                        Projectile.NewProjectile(NPC.GetSource_FromAI(), projPos, (Vector2.UnitY * 8.5f) + new Vector2(target.velocity.X, 0f), ModContent.ProjectileType<SmoggyNimbusRaindrop>(), NPC.damage, 0f, ai0: Main.rand.Next(3));
+                    }
+                }
+            }
+            else if (AIState == _AIState.Following)
+            {
+                if (--AITimer < -AIRand)
+                {
+                    AITimer = 0;
+                    NPC.netUpdate = true;
+                    AIState = _AIState.Spinning;
+                    NPC.velocity *= 0f;
+                }
+            }
+        }
+        public override bool? CanFallThroughPlatforms() => true;
     }
 }
---
+++ Content/Projectiles/Hostile/SmoggyNimbusRaindrop.cs
@@ -0,0 +1,65 @@
+﻿using ITD.Particles;
+using ITD.Particles.Misc;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.Graphics.Renderers;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Projectiles.Hostile
+{
+    public class SmoggyNimbusRaindrop : ModProjectile
+    {
+        public override string Texture => "ITD/Particles/Textures/LavaRainParticle";
+        public override void SetDefaults()
+        {
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.width = 12;
+            Projectile.height = 28;
+            Projectile.timeLeft = 120;
+            Projectile.hide = true;
+        }
+        public override void AI()
+        {
+            Dust d = Dust.NewDustPerfect(Projectile.Center, DustID.Torch);
+            d.noGravity = true;
+        }
+        public override void OnKill(int timeLeft)
+        {
+            int amountOfDusts = 6;
+            for (int i = 0; i < amountOfDusts; i++)
+            {
+                float angleRange = MathHelper.PiOver4;
+                float rotRadians = angleRange / (float)amountOfDusts;
+                Vector2 velocity = (angleRange - rotRadians).ToRotationVector2().RotatedBy(rotRadians * i) * 4f;
+                Dust.NewDust(Projectile.Bottom + Vector2.UnitX * Projectile.velocity.X, 1, 1, DustID.Torch, velocity.X, velocity.Y, Scale: 1.5f);
+            }
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.Lerp(Color.White, Color.SlateGray, (MathF.Sin((float)Main.timeForVisualEffects / 8f) + 1f) / 2f);
+        }
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCs.Add(index);
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            if (Projectile.localAI[0] < 1f)
+                Projectile.localAI[0] += 0.1f;
+            Texture2D glowTex = ModContent.Request<Texture2D>("ITD/Particles/Textures/LyteflyParticle_Glow").Value;
+            Texture2D tex = TextureAssets.Projectile[Type].Value;
+            Rectangle texFrame = tex.Frame(3, 1, (int)Projectile.ai[0]);
+            Vector2 texOrigin = new(tex.Width / 3 / 2, tex.Height / 2);
+            Color glowColor = (Color.OrangeRed with { A = 0 }) * 0.5f * Projectile.localAI[0];
+            Main.spriteBatch.Draw(glowTex, Projectile.Center - Main.screenPosition, null, glowColor, 0f, glowTex.Size() * 0.5f, 0.5f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(glowTex, Projectile.Center - Main.screenPosition, null, Color.Yellow with { A = 0 } * 0.4f * Projectile.localAI[0], 0f, glowTex.Size() * 0.5f, 0.2f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, texFrame, GetAlpha(Color.White).Value, Projectile.velocity.ToRotation() - MathHelper.PiOver2, texOrigin, Projectile.localAI[0] * 1.05f, SpriteEffects.None, 0f);
+            return false;
+        }
+    }
+}
