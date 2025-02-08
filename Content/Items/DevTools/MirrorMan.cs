036f7092ebc0f207f48c159baeaf250ef01866c6
queue <angeltrisix@gmail.com>
2025-02-07 20:05:19 -0500

    deep desert rubbles

---
+++ Content/Items/DevTools/MirrorMan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.Content.Items.DevTools
+{
+    public class MirrorMan : ModItem
+    {
+        public enum MirrorManState : byte
+        {
+            Select,
+            MirrorHorizontally,
+            MirrorVertically,
+        }
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 32;
+            Item.useTime = Item.useAnimation = 20;
+            Item.autoReuse = false;
+            Item.consumable = false;
+            Item.channel = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+    }
+}
---
+++ Content/Tiles/DeepDesert/Rubbles/PegmatitePlants.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.DataStructures;
+using Terraria.ObjectData;
+using Terraria.Enums;
+using Terraria.GameContent;
+using ITD.Content.Items.Placeable;
+
+namespace ITD.Content.Tiles.DeepDesert.Rubbles
+{
+    public class PegmatitePlant4x4 : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoFail[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.Width = 4;
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 18];
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.Table | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.addTile(Type);
+
+            FlexibleTileWand.RubblePlacementLarge.AddVariations(ModContent.ItemType<LightPyracotta>(), Type, 0);
+        }
+    }
+    public class PegmatitePlant3x2 : Example3x2RubbleBase
+    {
+        public override void SetStaticDefaultsSafe()
+        {
+            FlexibleTileWand.RubblePlacementLarge.AddVariations(ModContent.ItemType<LightPyracotta>(), Type, 0);
+        }
+    }
+    public class PegmatitePlant2x2 : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoFail[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.addTile(Type);
+
+            FlexibleTileWand.RubblePlacementLarge.AddVariations(ModContent.ItemType<LightPyracotta>(), Type, 0);
+        }
+    }
+}
