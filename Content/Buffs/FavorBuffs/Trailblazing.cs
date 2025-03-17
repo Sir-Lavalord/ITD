0c2c4fa0cb15f4406ed24d9035f4c11b2654dea1
Tapenki <113533673+Tapenki@users.noreply.github.com>
2025-03-17 21:44:00 +0300

    favors for real this time
    throwable guardian trail
    added trailblazer

---
+++ Content/Buffs/FavorBuffs/Trailblazing.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ITD.Content.Buffs.FavorBuffs
+{
+    public class Trailblazing : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoSave[Type] = false;
+            Main.debuff[Type] = false;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+			player.moveSpeed += 0.005f * player.buffTime[buffIndex];
+			player.DoBootsEffect(new Utils.TileActionAttempt(player.DoBootsEffect_PlaceFlamesOnTile));
+        }
+    }
+}
---
+++ Content/Items/Favors/Prehardmode/Trailblazer.cs
@@ -0,0 +1,75 @@
+﻿using ITD.Content.Projectiles.Friendly.Melee.Snaptraps;
+using ITD.Content.Projectiles.Friendly.Melee.Snaptraps.Extra;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+using ITD.Systems;
+using ITD.Content.Rarities;
+using ITD.Content.Buffs.FavorBuffs;
+
+namespace ITD.Content.Items.Favors.Prehardmode
+{
+    public class Trailblazer : Favor
+    {
+        public override int FavorFatigueTime => 60;
+        public override bool IsCursedFavor => false;
+        public override void SetFavorDefaults()
+        {
+            Item.width = Item.height = 32;
+        }
+        public override string GetBarStyle()
+        {
+            return "DefaultBarStyle";
+        }
+        public override string GetChargeSound()
+        {
+            return "DefaultChargeSound";
+        }
+        public override bool UseFavor(Player player)
+        {
+			if (player.direction == 1) {
+				if (player.velocity.X < 16f)
+					player.velocity.X = 16f;
+			}
+            else {
+				if (player.velocity.X > -16f)
+					player.velocity.X = -16f;
+			}
+			player.AddBuff(ModContent.BuffType<Trailblazing>(), 60 * 5);
+			
+			for (int i = 0; i < 15; i++)
+            {
+                int dust = Dust.NewDust(player.Center, 1, 1, DustID.Torch, 0f, 0f, 0, default, 4f);
+				Main.dust[dust].noGravity = true;
+				Main.dust[dust].velocity *= 5f;
+            }
+			SoundEngine.PlaySound(SoundID.Item45, player.Center);
+            return true;
+        }
+        public override float ChargeAmount(ChargeData chargeData)
+        {
+            if (chargeData.Type == ChargeType.DistanceTravelled && Math.Abs(chargeData.AmountX) > 2f)
+            {
+                return 0.002f;
+            }
+            return 0f;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            var line = tooltips.First(x => x.Name == "Tooltip3");
+            string hotkeyText = string.Format(line.Text, FavorPlayer.FavorKeybindString);
+            line.Text = hotkeyText;
+        }
+    }
+}
