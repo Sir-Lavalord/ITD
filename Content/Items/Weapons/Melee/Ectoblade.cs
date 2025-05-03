14d0bf4ec97d3a1b11bfaa35f0c0c698aa20143a
Tapenki <113533673+Tapenki@users.noreply.github.com>
2025-05-03 10:57:19 +0300

    ectoblade changes

--- Content/Items/Weapons/Melee/Ectoblade.cs
+++ Content/Items/Weapons/Melee/Ectoblade.cs
@@ -4,16 +4,21 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria.Audio;
+
 using ITD.Content.Projectiles.Friendly.Melee;
 using ITD.Systems;
 using ITD.Utilities;
-using System;
+using ITD.Particles.Misc;
+using ITD.Particles;
 
 namespace ITD.Content.Items.Weapons.Melee
 {
 	public class Ectoblade : ModItem
 	{
-		public int attackCycle = 0;
+		//public int attackCycle = 0;
+		public ParticleEmitter emitter;
 		
 		public override void SetStaticDefaults()
         {
@@ -38,22 +43,55 @@ namespace ITD.Content.Items.Weapons.Melee
 			Item.rare = ItemRarityID.Blue;
 			Item.UseSound = SoundID.Item15;
 			Item.autoReuse = true;
-			Item.shoot = ModContent.ProjectileType<GhostlyBlade>();
+			//Item.shoot = ModContent.ProjectileType<GhostlyBlade>();
 			Item.shootSpeed = 20;
 		}
 		
 		public override void MeleeEffects (Player player, Rectangle hitbox)
         {
-			Vector2 position;
-			Vector2 spinningpoint;
-			MiscHelpers.GetPointOnSwungItemPath(player, 60f, 60f, 0.2f + 0.8f * Main.rand.NextFloat(), player.GetAdjustedItemScale(Item), out position, out spinningpoint);
-			Vector2 value = spinningpoint.RotatedBy((double)(1.57079637f * (float)player.direction * player.gravDir), default(Vector2));
-			Dust.NewDustPerfect(position, 180, new Vector2?(value * 4f), 100, default(Color), 1.5f).noGravity = true;
+			if (player.itemAnimation == player.itemAnimationMax)
+			{
+				emitter = ParticleSystem.NewEmitter<EctoCloud>(ParticleEmitterDrawCanvas.WorldUnderProjectiles);
+				emitter.tag = Item;
+				emitter.keptAlive = true;
+			}
+			
+			if (emitter != null)
+			{
+                emitter.keptAlive = true;
+				for (int j = 0; j < 2; j++) {
+					Vector2 position;
+					Vector2 spinningpoint;
+					MiscHelpers.GetPointOnSwungItemPath(player, 60f, 60f, 0.5f + 0.6f * Main.rand.NextFloat(), player.GetAdjustedItemScale(Item), out position, out spinningpoint);
+					Vector2 velocity = spinningpoint.RotatedBy((double)(1.57079637f * (float)player.direction * player.gravDir), default(Vector2));
+					emitter?.Emit(position, velocity * 4f, 0f, 20);
+				}
+			}
         }
 		
-		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
-			attackCycle = ++attackCycle % 2;
-			return attackCycle == 1;
+		//public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {			
+		//	attackCycle = ++attackCycle % 2;
+		//	return attackCycle == 1;
+		//}
+		
+		public override void ModifyHitNPC(Player player, NPC target, ref NPC.HitModifiers modifiers)
+		{
+			if (player.statMana >= player.statManaMax2 * 0.5f)
+			{
+				SoundEngine.PlaySound(SoundID.NPCDeath39, target.Center);
+				ParticleEmitter hitEmitter = ParticleSystem.NewEmitter<EctoCloud>(ParticleEmitterDrawCanvas.WorldUnderProjectiles);
+				hitEmitter.tag = Item;
+				hitEmitter.keptAlive = true;
+				
+				for (int j = 0; j < 15; j++) {
+					hitEmitter?.Emit(target.Center, Main.rand.NextVector2Circular(4f, 12f), 0f, 20);
+				}
+			}
+			
+			modifiers.FlatBonusDamage += player.GetTotalDamage(DamageClass.Magic).ApplyTo(player.statMana) * 0.5f;
+			player.manaRegenDelay = player.maxRegenDelay;
+			player.statMana = 0;
+
 		}
 		
 		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
---
+++ Particles/Misc/EctoCloud.cs
@@ -0,0 +1,58 @@
+﻿using ITD.Content.Items.Dyes;
+using ITD.Content.Projectiles.Unused;
+using ITD.Utilities;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ITD.Particles.Misc
+{
+    public class EctoCloud : ParticleEmitter
+    {
+        /*
+        public override void SetDefaults()
+        {
+            canvas = ParticleEmitterDrawCanvas.WorldUnderProjectiles;
+            scale *= 1.3f;
+            timeLeft = 60;
+        }
+        */
+        public override void OnEmitParticle(ref ITDParticle particle)
+        {
+            particle.scale *= 2f;
+            // timeleft is now set on Emit() call. canvas is set on Emitter initialization.
+        }
+        public override void AI(ref ITDParticle particle)
+        {
+            particle.scale = particle.spawnParameters.Scale * particle.ProgressOneToZero;
+            particle.velocity *= 0.95f;
+        }
+        public override Color GetAlpha(ITDParticle particle) => Color.White;
+        public override void DrawAllParticles()
+        {
+            Texture2D tex = ModContent.Request<Texture2D>("ITD/Particles/Textures/EctoCloud").Value;
+            Color color1 = new(9, 121, 255);
+            Color color2 = new(0, 220, 255);
+			Color color3 = new(255, 255, 255);
+            for (int i = 0; i < particles.Count; i++)
+            {
+                particles[i].DrawCommon(Main.spriteBatch, tex, CanvasOffset, color1);
+            }
+			for (int i = 0; i < particles.Count; i++)
+            {
+                particles[i].DrawCommon(Main.spriteBatch, tex, CanvasOffset, color2, null, null, null, particles[i].scale * 0.8f);
+            }
+			for (int i = 0; i < particles.Count; i++)
+            {
+                particles[i].DrawCommon(Main.spriteBatch, tex, CanvasOffset, color3, null, null, null, particles[i].scale * 0.6f);
+            }
+        }
+    }
+}
