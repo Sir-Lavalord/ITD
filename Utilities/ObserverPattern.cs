b24f740adebeca9bf9b9cf5fe6f5238dd92c388f
Guluere <32575902+Guluere@users.noreply.github.com>
2025-05-17 02:08:20 +0800

    Create ObserverPattern.cs
    Untested, probably not MP friendly either.
    
    The message version allows you to send strings. The Event version allows you to hook functions.

---
+++ Utilities/ObserverPattern.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITD.Utilities.ObserverPatterns
+{
+    public interface I_ITDMessageInterface
+    {
+        void RecievingMessage(string message);
+    }
+
+    public class ITDMessageInterfaceGroup
+    {
+        private readonly List<I_ITDMessageInterface> _observers = new List<I_ITDMessageInterface>();
+
+        public void AddListener(I_ITDMessageInterface i_ITDMessageInterface)
+        {
+            _observers.Add(i_ITDMessageInterface);
+        }
+
+        public void RemoveListener(I_ITDMessageInterface i_ITDMessageInterface)
+        {
+            _observers.Remove(i_ITDMessageInterface);
+        }
+
+        public void SendMessage(string message)
+        {
+            foreach (I_ITDMessageInterface i_ in _observers)
+            {
+                i_.RecievingMessage(message); 
+            }
+        }
+    }
+
+    public class ITDListenerEvent<T>
+    {
+        private event Action<T> listeners;
+
+        public void AddListener(Action<T> listener)
+        {
+            listeners += listener;
+        }
+
+        public void RemoveListener(Action<T> listener)
+        {
+            listeners -= listener;
+        }
+
+        public void Invoke(T value)
+        {
+            listeners?.Invoke(value);
+        }
+
+        public void RemoveAllListeners()
+        {
+            listeners = null;
+        }
+    }
+}
+
+/// <summary>
+/// Below are mostly example code for its usage.
+/// </summary>
+private class Test
+{
+    ITD.Utilities.ObserverPatterns.ITDListenerEvent<int> TestEvent = new ITD.Utilities.ObserverPatterns.ITDListenerEvent<int>();
+    ITD.Utilities.ObserverPatterns.ITDMessageInterfaceGroup TestMessageGroup = new ITD.Utilities.ObserverPatterns.ITDMessageInterfaceGroup();
+
+    public void TestFunction(int i)
+    {
+
+    }
+
+    public void EventFunction()
+    {
+        TestEvent.AddListener(TestFunction);
+        TestEvent.RemoveListener(TestFunction);
+    }
+}
