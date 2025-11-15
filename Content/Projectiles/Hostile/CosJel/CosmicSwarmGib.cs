5ea9d3771a3479d564420a57f5881d0db1c14ec0
mirrorman <135953905+A5torm@users.noreply.github.com>
2025-11-16 03:36:12 +0700

    cosjel attack 8 wip

--- Content/Projectiles/Hostile/CosJel/CosmicSwarmGib.cs
+++ Content/Projectiles/Hostile/CosJel/CosmicSwarmGib.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ITD.Content.Dusts;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.GameContent;
@@ -21,8 +23,8 @@ public class CosmicSwarmGib : ModProjectile
     public override void SetStaticDefaults()
     {
         ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5; // The length of old position to be recorded
-        ProjectileID.Sets.TrailingMode[Projectile.type] = 0; // The recording mode
-        Main.projFrames[Projectile.type] = 2;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2; // The recording mode
+        Main.projFrames[Projectile.type] = 1;
     }
     public override Color? GetAlpha(Color lightColor)
     {
@@ -34,7 +36,6 @@ public class CosmicSwarmGib : ModProjectile
     }
     public override void OnSpawn(IEntitySource source)
     {
-        Projectile.frame = Main.rand.Next(0, 2);
     }
     public override void OnKill(int timeLeft)
     {
@@ -47,16 +48,62 @@ public class CosmicSwarmGib : ModProjectile
 
     public override void AI()
     {
-        Projectile.rotation += 0.2f;
+        if (Main.rand.NextBool(2))
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                int d = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.OrangeTorch, 0f, 0f, 0, default(Color), 2f);
+                Main.dust[d].noGravity = true;
+                Main.dust[d].noLight = true;
+                Main.dust[d].velocity *= 4f;
+            }
+        }
+        Projectile.rotation = Projectile.velocity.ToRotation();
     }
     public override bool PreDraw(ref Color lightColor)
     {
-        Player player = Main.player[Projectile.owner];
-        Texture2D effectTexture = TextureAssets.Extra[ExtrasID.SharpTears].Value;
+        Texture2D tex = TextureAssets.Projectile[Type].Value;
+        Texture2D effectTexture = TextureAssets.Extra[ExtrasID.StardustJellyfishSmall].Value;
         Vector2 effectOrigin = effectTexture.Size() / 2f;
-        lightColor = Lighting.GetColor((int)player.Center.X / 16, (int)player.Center.Y / 16);
+        Rectangle frame = tex.Frame(1, Main.projFrames[Type], 0, Projectile.frame);
+        Vector2 center = Projectile.Size / 2f;
+        for (int i = Projectile.oldPos.Length - 1; i > 0; i--)
+        {
+            Projectile.oldRot[i] = Projectile.oldRot[i - 1];
+            Projectile.oldRot[i] = Projectile.rotation + MathHelper.PiOver2;
 
-        Main.EntitySpriteDraw(effectTexture, Projectile.Center, new Rectangle?(Projectile.Hitbox), new Color(120, 184, 255, 50) * 0.05f * Projectile.timeLeft, Projectile.rotation, effectOrigin, Projectile.scale, SpriteEffects.None, 0f);
-        return true;
+        }
+        Vector2 miragePos = Projectile.position - Main.screenPosition + center;
+        Vector2 origin = new(tex.Width * 0.5f, tex.Height / Main.projFrames[Type] * 0.5f);
+        float time = Main.GlobalTimeWrappedHourly;
+        float timer = (float)Main.time / 240f + time * 0.04f;
+        Main.EntitySpriteDraw(effectTexture, miragePos - new Vector2 (24,0).RotatedBy(Projectile.rotation), null, new Color(90, 70, 200, 50) * Projectile.Opacity, Projectile.rotation + MathHelper.PiOver2, effectOrigin, Projectile.scale * 1.25f, SpriteEffects.None, 0);
+
+        time %= 4f;
+        time /= 2f;
+
+        if (time >= 1f)
+        {
+            time = 2f - time;
+        }
+
+        time = time * 0.5f + 0.5f;
+
+        for (float i = 0f; i < 1f; i += 0.35f)
+        {
+            float radians = (i + timer) * MathHelper.TwoPi;
+
+            Main.EntitySpriteDraw(tex, miragePos + new Vector2(0f, 6).RotatedBy(radians) * time, frame, new Color(255, 114, 70, 50) * Projectile.Opacity, Projectile.rotation, origin, Projectile.scale, SpriteEffects.None, 0);
+        }
+
+        for (float i = 0f; i < 1f; i += 0.5f)
+        {
+            float radians = (i + timer) * MathHelper.TwoPi;
+
+            Main.EntitySpriteDraw(tex, miragePos + new Vector2(0f, 8).RotatedBy(radians) * time, frame, new Color(255, 114, 70, 50) * Projectile.Opacity, Projectile.rotation, origin, Projectile.scale, SpriteEffects.None, 0);
+        }
+
+        Main.EntitySpriteDraw(tex, miragePos, frame, Color.White * Projectile.Opacity, Projectile.rotation, origin, Projectile.scale, SpriteEffects.None, 0);
+        return false;
     }
 }
---
+++ Content/Projectiles/Hostile/CosJel/CosmicSwordStar2.cs
@@ -0,0 +1,127 @@
+﻿
+using ITD.Content.Dusts;
+using ITD.Content.NPCs.Bosses;
+using ITD.Content.Projectiles.Friendly.Melee;
+using ITD.Utilities;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.Graphics;
+using Terraria.Graphics.Shaders;
+
+namespace ITD.Content.Projectiles.Hostile.CosJel;
+
+public class CosmicSwordStar2 : ModProjectile
+{
+    public VertexStrip TrailStrip = new();
+
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 40;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+    }
+    public override string Texture => ITD.BlankTexture;
+
+    public override void SetDefaults()
+    {
+        Projectile.width = 30;
+        Projectile.height = 30;
+        Projectile.aiStyle = -1;
+        Projectile.hostile = true;
+        Projectile.penetrate = 1;
+        Projectile.timeLeft = 6000;
+        Projectile.ignoreWater = true;
+        Projectile.tileCollide = false;
+        Projectile.alpha = 0;
+
+    }
+    public ref float rotation =>  ref Projectile.ai[0];
+    public bool getGoing =>  Projectile.ai[1] != 0;
+    public float startTime => Projectile.ai[2];
+
+    public override void OnSpawn(IEntitySource source)
+    {
+    }
+    public override Color? GetAlpha(Color lightColor)
+    {
+        return Color.White * Projectile.Opacity;
+    }
+    public override bool? CanDamage()
+    {
+        return spawnGlow <= 0;
+    }
+    public override void AI()
+    {
+        if (Projectile.localAI[0]++ == startTime)
+        {
+            Projectile.velocity = Projectile.rotation.ToRotationVector2() * 2;
+            Projectile.ai[1]++;
+            spawnGlow = 1; 
+            for (int i = 0; i < 20; i++)
+            {
+                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<CosJelDust>(), 0, 0, 0, default, 2f);
+                Main.dust[dust].noGravity = true;
+                Main.dust[dust].velocity = Vector2.UnitX.RotatedByRandom(Math.PI) * Main.rand.NextFloat(0.9f, 1.1f) * 10;
+            }
+        }
+        if (getGoing)
+        {
+            Projectile.velocity *= 1.05f;
+        }
+        else
+        {
+            Projectile.velocity = Vector2.Zero;
+        }
+
+        spawnGlow -= 0.05f;
+    }
+    private Color StripColors(float progressOnStrip)
+    {
+        return new Color(90, 70, 255, 10);
+    }
+    private float StripWidth(float progressOnStrip)
+    {
+        return MathHelper.Lerp(10f, 2f, Utils.GetLerpValue(0f, 0.6f, progressOnStrip, true)) * Utils.GetLerpValue(0f, 0.07f, progressOnStrip, true);
+    }
+    float scaleX = 0.75f;
+    float scaleY = 2f;
+    float spawnGlow = 0;
+    public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+    {
+        Main.instance.DrawCacheProjsBehindNPCs.Add(index);
+
+    }
+    public override bool PreDraw(ref Color lightColor)
+    {
+
+        GameShaders.Misc["LightDisc"].Apply(null);
+        TrailStrip.PrepareStrip(Projectile.oldPos, Projectile.oldRot, StripColors, StripWidth, Projectile.Size * 0.5f - Main.screenPosition, Projectile.oldPos.Length, true);
+        TrailStrip.DrawTrail();
+
+        Main.pixelShader.CurrentTechnique.Passes[0].Apply();
+    
+        Player player = Main.player[Projectile.owner];
+        Texture2D effectTexture = TextureAssets.Extra[ExtrasID.SharpTears].Value;
+        Vector2 effectOrigin = effectTexture.Size() / 2f;
+        lightColor = Lighting.GetColor((int)player.Center.X / 16, (int)player.Center.Y / 16);
+        Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+        Main.EntitySpriteDraw(effectTexture, drawPosition + new Vector2(-20, 0).RotatedBy(Projectile.rotation), null, new Color(255, 255, 255, 40), Projectile.rotation, effectTexture.Size() / 2f, new Vector2(scaleX, scaleX), SpriteEffects.None, 0);
+        Main.EntitySpriteDraw(effectTexture, drawPosition, null, new Color(90, 70, 255, 30), Projectile.rotation + MathHelper.PiOver2, effectTexture.Size() / 2f, new Vector2(scaleX, scaleY), SpriteEffects.None, 0);
+        Main.EntitySpriteDraw(effectTexture, drawPosition + new Vector2(20, 0).RotatedBy(Projectile.rotation), null, new Color(255, 255, 255, 127), Projectile.rotation + MathHelper.PiOver2, effectTexture.Size() / 2f, new Vector2(0.2f, 3), SpriteEffects.None, 0);
+
+        if (spawnGlow > 0)//fargo eridanus epic
+        {
+            float scale = 3f * Projectile.scale * (float)Math.Cos(Math.PI / 2 * spawnGlow);
+            float opacity = Projectile.Opacity * (float)Math.Sqrt(spawnGlow);
+            Main.EntitySpriteDraw(effectTexture, drawPosition + new Vector2(-20, 0).RotatedBy(Projectile.rotation), null, new Color(255, 255, 255, 127) * opacity, Projectile.rotation, effectTexture.Size() / 2f, new Vector2(scaleX, scaleX) * scale, SpriteEffects.None, 0);
+            Main.EntitySpriteDraw(effectTexture, drawPosition, null, new Color(90, 70, 255, 50) * opacity, Projectile.rotation + MathHelper.PiOver2, effectTexture.Size() / 2f, new Vector2(scaleX, scaleY) * scale, SpriteEffects.None, 0);
+        }
+        return false;
+    }
+}
+
